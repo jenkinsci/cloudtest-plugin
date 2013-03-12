c4818e9933e9bb4354afee47984eb15ec9d65d2d
Matt Solnit <msolnit@soasta.com>
2013-03-12 09:52:44 -0700

    In the "Import CloudTest Objects" build step, move the conflict mode drop-down out of Advanced, since it's a choice we want to make sure the user is aware of.

--- src/main/resources/com/soasta/jenkins/ImportFiles/config.groovy
+++ src/main/resources/com/soasta/jenkins/ImportFiles/config.groovy
@@ -8,14 +8,14 @@ if (descriptor.showUrlField()) {
     }
 }
 f.entry(title:"File(s) to import",field:"files") {
-    f.textbox()
+    f.expandableTextbox()
+}
+f.entry(title:"Conflict resolution mode",field:"mode") {
+    f.select(name:"mode")
 }
 f.advanced {
     f.entry(title:"Excludes",field:"excludes") {
         f.textbox()
-    }
-    f.entry(title:"Conflict resolution mode",field:"mode") {
-        f.select(name:"mode")
     }
     f.entry(title:"Additional Options",field:"additionalOptions") {
         f.expandableTextbox()
