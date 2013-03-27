15970807e4444f12dc2ef5f4e3102ab9dc9e7b5c
Matt Solnit <msolnit@soasta.com>
2013-03-26 20:51:59 -0700

    SCommand-based build steps (e.g. Play Composition) now use the Jenkins proxy settings, when appropriate.

--- src/main/java/com/soasta/jenkins/AbstractSCommandBuilder.java
+++ src/main/java/com/soasta/jenkins/AbstractSCommandBuilder.java
@@ -5,9 +5,13 @@
 package com.soasta.jenkins;
 
 import java.io.IOException;
+import java.util.regex.Pattern;
+
+import jenkins.model.Jenkins;
 
 import hudson.AbortException;
 import hudson.FilePath;
+import hudson.ProxyConfiguration;
 import hudson.model.AbstractBuild;
 import hudson.model.BuildListener;
 import hudson.tasks.Builder;
@@ -54,6 +58,41 @@ public abstract class AbstractSCommandBuilder extends Builder {
           .add("username="+s.getUsername())
           .addMasked("password=" + s.getPassword());
       
+      ProxyConfiguration proxyConfig = Jenkins.getInstance().proxy;
+
+      if (proxyConfig != null && proxyConfig.name != null) {
+          // Jenkins is configured to use a proxy server.
+
+          // Extract the destination CloudTest host.
+          String host = s.getUrl().getHost();
+
+          // Check if the proxy applies for this destination host.
+          // This code is more or less copied from ProxyConfiguration.createProxy() :-(.
+          boolean isNonProxyHost = false;
+          for (Pattern p : proxyConfig.getNoProxyHostPatterns()) {
+              if (p.matcher(host).matches()) {
+                  // It's a match.
+                  // Don't use the proxy.
+                  isNonProxyHost = true;
+
+                  // No need to continue checking the list.
+                  break;
+              }
+          }
+
+          if (!isNonProxyHost) {
+              // Add the SCommand proxy parameters.
+              args.add("httpproxyhost=" + proxyConfig.name)
+                  .add("httpproxyport=" + proxyConfig.port);
+
+              // If there are proxy credentials, add those too.
+              if (proxyConfig.getUserName() != null) {
+                  args.add("httpproxyusername=" + proxyConfig.getUserName())
+                      .addMasked("httpproxypassword=" + proxyConfig.getPassword());
+              }
+          }
+      }
+
       return args;
     }
 }
