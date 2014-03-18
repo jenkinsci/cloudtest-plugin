d1857b4c432a2f649f31b4dd58f08ce2109ac0b3
jzyang <jyang@soasta.com>
2014-03-18 16:30:32 -0700

    Updated Input Type, help files, and replaced APP File with APP Bundle.

--- src/main/java/com/soasta/jenkins/MakeAppTouchTestable.java
+++ src/main/java/com/soasta/jenkins/MakeAppTouchTestable.java
@@ -26,8 +26,6 @@ import org.kohsuke.stapler.QueryParameter;
 import java.io.IOException;
 import java.util.logging.Logger;
 
-import com.soasta.jenkins.makeAppTouchTestable.InputType;
-
 /**
  * @author Kohsuke Kawaguchi
  */
@@ -185,5 +183,39 @@ public class MakeAppTouchTestable extends Builder {
         }
     }
 
+    static enum InputType {
+      
+      APP ("-appbundle"),
+      IPA ("-ipa"),
+      PROJECT ("-project");
+    
+      String inputType;
+    
+      private InputType(String inputType) {
+          this.inputType = inputType;
+      }
+      
+      public String getInputType() {
+          return inputType;
+      }
+    
+      public static InputType getInputType(String inputType) {
+          // In case there were no input type provided, the default is
+          // always returned: PROJECT.
+          if (inputType == null) {
+              return InputType.PROJECT;
+          }
+          
+          try {
+              return InputType.valueOf(inputType);
+          }
+          catch (IllegalArgumentException e) {
+              // The input type passed in was bad so we will
+              // use the default type instead.
+              return InputType.PROJECT;
+          }
+      }
+    }
+    
     private static final Logger LOGGER = Logger.getLogger(MakeAppTouchTestable.class.getName());
 }
