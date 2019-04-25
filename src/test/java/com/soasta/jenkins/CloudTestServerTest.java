db56b482ce65c69bcd47a2564193192642252a27
rkritzer <rkritzer@akamai.com>
2019-04-25 11:46:01 -0700

    Un-comments test case for future development when servers are fixed

--- src/test/java/com/soasta/jenkins/CloudTestServerTest.java
+++ src/test/java/com/soasta/jenkins/CloudTestServerTest.java
@@ -17,48 +17,48 @@ import static org.junit.Assert.*;
 /**
  * @author Kohsuke Kawaguchi
  */
-public class CloudTestServerTest {
+public class CloudTestServerTest extends HudsonTestCase {
     @Inject
     CloudTestServer.DescriptorImpl descriptor;
     private CloudTestServer aServer;
 
-//    @Override
-//    protected void setUp() throws Exception {
-//        super.setUp();
-//        aServer = new CloudTestServer("http://35.165.9.6/concerto/", "Anonymous", Secret.fromString("password"), "utest", "Unit Test Server", "", "", Secret.fromString("secret"), false);
-//    }
-//
-//    public void testValidate() throws Exception {
-//        FormValidation f = aServer.validate();
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        aServer = new CloudTestServer("http://experimental.soasta.com/", "Anonymous", Secret.fromString("password"), "utest", "Unit Test Server", "", "", Secret.fromString("secret"), false);
+    }
+
+    public void testValidate() throws Exception {
+        FormValidation f = aServer.validate();
+        assertThat(f.kind, is(Kind.OK));
+
+//        f = new CloudTestServer("http://testdrive.soasta.com/", "abc", Secret.fromString("def")).validate();
 //        assertThat(f.kind, is(Kind.OK));
-//
-////        f = new CloudTestServer("http://testdrive.soasta.com/", "abc", Secret.fromString("def")).validate();
-////        assertThat(f.kind, is(Kind.OK));
-//    }
-//
-//    public void testBuildNumber() throws IOException {
-//        VersionNumber b = aServer.getBuildNumber();
-//        System.out.println(b);
-//        assertTrue(b.compareTo(new VersionNumber("5"))>=0);
-//    }
-//
-//    public void testMissingID() throws IOException {
-//        CloudTestServer s = new CloudTestServer("http://foo/", "joe", Secret.fromString("secret"), null, "Name", "", "", Secret.fromString("secret"), false);
-//        assertNotNull("ID should have been automatically generated.", s.getId());
-//        assertTrue("ID should be non-empty.", s.getId().trim().length() > 0);
-//    }
-//
-//    public void testMissingName() throws IOException {
-//        CloudTestServer s = new CloudTestServer("http://foo/", "joe", Secret.fromString("secret"), "id", null, "", "", Secret.fromString("secret"), false);
-//        assertNotNull("Name should have been automatically generated.", s.getName());
-//        assertEquals("Incorrect name.", s.getUrl() + " (" + s.getUsername() + ")", s.getName());
-//    }
-//
-//    public void testConfigRoundtrip() throws Exception {
-//        jenkins.getInjector().injectMembers(this);
-//        CloudTestServer before = new CloudTestServer("http://abc/", "def", Secret.fromString("ghi"), "testid", "Test Name", "", "", Secret.fromString("secret"), false);
-//        descriptor.setServers(Collections.singleton(before));
-//        configRoundtrip();
-//        assertEqualDataBoundBeans(before, descriptor.getServers().get(0));
-//    }
+    }
+
+    public void testBuildNumber() throws IOException {
+        VersionNumber b = aServer.getBuildNumber();
+        System.out.println(b);
+        assertTrue(b.compareTo(new VersionNumber("5"))>=0);
+    }
+
+    public void testMissingID() throws IOException {
+        CloudTestServer s = new CloudTestServer("http://foo/", "joe", Secret.fromString("secret"), null, "Name", "", "", Secret.fromString("secret"), false);
+        assertNotNull("ID should have been automatically generated.", s.getId());
+        assertTrue("ID should be non-empty.", s.getId().trim().length() > 0);
+    }
+
+    public void testMissingName() throws IOException {
+        CloudTestServer s = new CloudTestServer("http://foo/", "joe", Secret.fromString("secret"), "id", null, "", "", Secret.fromString("secret"), false);
+        assertNotNull("Name should have been automatically generated.", s.getName());
+        assertEquals("Incorrect name.", s.getUrl() + " (" + s.getUsername() + ")", s.getName());
+    }
+
+    public void testConfigRoundtrip() throws Exception {
+        jenkins.getInjector().injectMembers(this);
+        CloudTestServer before = new CloudTestServer("http://abc/", "def", Secret.fromString("ghi"), "testid", "Test Name", "", "", Secret.fromString("secret"), false);
+        descriptor.setServers(Collections.singleton(before));
+        configRoundtrip();
+        assertEqualDataBoundBeans(before, descriptor.getServers().get(0));
+    }
 }
